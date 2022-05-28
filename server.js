4f18285d0dfa451feb3f509fed9fe0e9f6c76ba1
salem z <salem.zerezghi2@gmail.com>
2022-05-27 23:07:15 -0400

    boiler plate

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+console.log(PORT)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
